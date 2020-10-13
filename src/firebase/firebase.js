a0f8d90622ad22481459b9687c27723a91561911
Alec Khoury <akhoury.ak@gmail.com>
2020-10-13 08:25:35 -0500

    Add firebase auth routes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,7 @@
 import app from "firebase/app";
 import "firebase/auth";
 import "firebase/store";
+import { register } from "../serviceWorker";
 
 import firebaseConfig from "./config";
 
@@ -10,6 +11,26 @@ class Firebase {
     this.app = app;
     this.auth = app.auth();
     this.db = app.firestore();
+  }
+
+  async register(name, email, password) {
+    const newUser = await this.auth.createUserWithEmailAndPassword(
+      email,
+      password
+    );
+    return newUser.user.updateProfile({ displayName: name });
+  }
+
+  login(email, password) {
+    return this.auth.signIngWithEmailAndPassword(email, password);
+  }
+
+  logout() {
+    return this.auth.signOut();
+  }
+
+  resetPassword(email) {
+    return this.auth.sendPasswordResetEmail(email);
   }
 }
 
